f85b76a1d49ecbd394dddf8f34a78e3a7649de0c
Elijah Insua <tmpvar@gmail.com>
2012-07-11 23:50:24 -0700

    Add: midi velocity is now bounded to 0-127 and pads light #0F0 when pressed

--- command/midi/index.js
+++ command/midi/index.js
@@ -7,24 +7,38 @@ output.openPort(0);
 
 var state = {0:false, 1:false, 2: false, 3: false };
 
-var handlePress = function(pad) {
-  if (!state[pad.index]) {
-    output.sendMessage([144, 60+pad.index, (pad.value/18000)*127]);
-    state[pad.index] = true
-  }
-};
-
-var handleDepress = function(pad) {
-  if (state[pad.index]) {
-    output.sendMessage([128, 60+pad.index, (pad.value/18000)*127]);
-    state[pad.index] = false;
-  }
-};
-
 module.exports = function(tpad) {
-  tpad.each(function(pad) {
-    pad.on('press', handlePress);
-    pad.on('depress', handleDepress);
 
+  var handlePress = function(pad) {
+    if (!state[pad.index]) {
+      var velocity = Math.floor(63.5 + ((pad.value-tpad.activationThreshold)/(18000-tpad.activationThreshold))*63.5);
+
+      if (velocity > 127) { velocity = 127; }
+
+      output.sendMessage([
+        144,
+        60+pad.index,
+        velocity
+      ]);
+
+      state[pad.index] = true
+      pad.color('#00FF00');
+      pad.once('depress', handleDepress);
+    }
+  };
+
+  var handleDepress = function(pad) {
+    if (state[pad.index]) {
+
+      output.sendMessage([128, 60+pad.index, 127]);
+
+      state[pad.index] = false;
+      pad.color('#000000');
+      pad.once('press', handlePress);
+    }
+  };
+
+  tpad.each(function(pad, i) {
+    pad.once('press', handlePress);
   });
 };
