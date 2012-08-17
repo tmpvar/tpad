f7bca2afe08d36cd90290dc4665584e6bc27f8dd
Elijah Insua <tmpvar@gmail.com>
2012-08-16 18:10:36 -0700

    Add: 'tpad flash' command which upgrades firmware without any external dependencies

--- firmware/index.js
+++ firmware/index.js
@@ -7,7 +7,9 @@ var
   tar     = require('tar'),
   zlib    = require('zlib'),
   exec    = require('child_process').exec,
-  semver  = require('semver');
+  semver  = require('semver'),
+  lufacdc = require('chip.avr.lufacdc'),
+  SerialPort = require('serialport').SerialPort;
 
 function error() {
   console.log(arguments);
@@ -20,96 +22,71 @@ var firmware = module.exports = function(options) {
 
   // TODO: ensure you can specify a firmware dir
   if (!options.location) {
-    firmware.fetch(function(err, folder) {
-      firmware.build(folder, function(err) {
-        firmware.flash(folder, options);
-      });
+    firmware.fetch(options, function(err, hexData) {
+      firmware.flash(hexData, options);
     });
   } else {
     console.log('Using firmware', path.resolve(options.location));
-    firmware.build(firmwareFolder, function(err) {
+    firmware.build(firmwareFolder, options, function(err) {
       firmware.flash(firmwareFolder, options);
     })
   }
 };
 
 
-firmware.fetch = function(fn) {
+firmware.fetch = function(options, fn) {
 
   // get the latest firmware
   console.log('finding the latest firmware');
   request('https://api.github.com/repos/tmpvar/tpad-firmware/git/refs/tags', function(err, res) {
-    var
-      data    = JSON.parse(res.body),
-      first   = true,
-      passed  = 0,
-      where   = 0,
-      latest, sha, shaDir, size, url, req;
+    var data    = JSON.parse(res.body);
 
     // find the latest version
     data.sort(function(a, b) {
       return (semver.gt(a, b)) ? -1 : 1;
     });
 
-    latest = data[0];
-    sha = latest.object.sha.substring(0,7);
-    url = 'https://github.com/tmpvar/tpad-firmware/tarball/' + sha;
-    shaDir = __dirname + '/tmpvar-tpad-firmware-' + sha
+    var latest = data[0];
+    var tag = latest.ref.replace('refs/tags/', '');
+    var tpad = options.tpad.name;
 
-    fs.exists(shaDir, function(exists) {
-      if (!exists) {
-        req = request(url, sha);
+    var url = 'https://raw.github.com/tmpvar/tpad-firmware/' + tag +
+          '/tpad/' + tpad + '/' + tpad + '.hex';
 
-        console.log('found ' + latest.ref.replace('refs/tags/',''));
-
-        console.log('downloading and extracting...');
-
-        req.on('data', function(data) {
-          passed += data.length;
-          var pos = Math.floor((passed/size)*100)/10;
-
-          if (first) {
-            size = req.response.headers['content-length'];
-            first = false;
-
-          // update progress
-          } else if (Math.floor(pos) > where){
-            process.stdout.write('#');
-            where = pos;
-          }
-        });
-
-        req.pipe(zlib.Unzip())
-          .pipe(tar.Extract({ path: __dirname })).on('end', function() {
-            console.log(' done');
-            fn(null, shaDir);
-          });
-
-      } else {
-        fn(null, shaDir);
+    console.log('fetching', url)
+    request(url, function(err, res) {
+      if (err) {
+        return fn(err);
       }
+
+      fn(null, res.body.toString());
     });
   });
 };
 
 // build all of the firmware
-firmware.build = function(dir, fn) {
-  console.log('building...');
+firmware.build = function(dir, options, fn) {
 
-  exec('make', {
-    cwd : dir
-  }, function (error, stdout, stderr) {
-    if (error) {
-      throw error;
-    } else {
-      fn();
-    }
-  });
+  if (options.build) {
+    console.log('building...');
+
+    exec('make', {
+      cwd : dir
+    }, function (error, stdout, stderr) {
+      if (error) {
+        throw error;
+      } else {
+        fn();
+      }
+    });
+  } else {
+    fn();
+  }
 };
 
 // Basically wait for the known tpad connection to drop
 // wait 2 seconds then run avrdude
-firmware.waitForReset = function(dir, options, fn) {
+firmware.waitForReset = function(options, fn) {
 
   // attempt a software reset (as of firmware 0.0.2)
   if (options.tpad.version && semver.gte(options.tpad.version, '0.0.2')) {
@@ -129,14 +106,9 @@ firmware.waitForReset = function(dir, options, fn) {
         state = 1;
         setTimeout(tick, 100);
       } else if (state === 1) {
-        console.log('\ntpad is booting..');
-
-        // we don't want to mess with the pad while it's being flashed
-        // so close the serial connection for good measure.
         options.serialport.close();
-
         setTimeout(function() {
-          fn(dir + '/tpad/' + options.tpad.name);
+          fn();
         }, 500);
       } else {
         setTimeout(tick, 100);
@@ -145,19 +117,41 @@ firmware.waitForReset = function(dir, options, fn) {
   }, 100);
 };
 
-firmware.flash = function(dir, options, fn) {
-  firmware.waitForReset(dir,options, function(tpadFirmwareDir) {
-    console.log('flashing...');
-    var env = process.env;
-    env.AVRDUDE_PORT = options.serialport.port;
+firmware.flash = function(hexData, options, fn) {
+  firmware.waitForReset(options, function(err) {
+    if (err) {
+      fn && fn(err);
+      return;
+    }
 
-    exec('make avrdude', {
-      cwd : tpadFirmwareDir,
-      env : env
-    }, function (err, stdout, stderr) {
-      if (err) { throw err; }
-      console.log('OK!')
-      process.exit();
+    var sp = new SerialPort(options.serialport.port);
+
+    sp.on('open', function() {
+      lufacdc.init(sp, function (err, flasher) {
+        if (err) {
+          throw err;
+        }
+
+        flasher.erase(function() {
+          console.log('initialized');
+
+          flasher.program(hexData, function(err) {
+            if (err) throw err;
+            console.log('programmed!');
+
+            flasher.verify(function(err) {
+              if (err) {
+                throw err
+              }
+              flasher.fuseCheck(function(err) {
+                if (err) throw err;
+                console.log('OK!');
+                process.exit();
+              });
+            });
+          });
+        });
+      });
     });
   });
 };
